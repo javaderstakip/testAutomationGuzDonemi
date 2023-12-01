1f24b84b3d2f9778cfb6b4c091fd4c9dacc2e42b
javaderstakip <122124230+javaderstakip@users.noreply.github.com>
2023-12-01 22:56:36 +0300

    Update Home.java
    01.12.2023 tarihinde akşam yaptığım çalışmalr.

--- src/main/java/org/ta/Home.java
+++ src/main/java/org/ta/Home.java
@@ -1,7 +1,50 @@
-package org.ta;
+package org.example;
 
-public class Home {
-    public static void main(String[] args) {
-        System.out.println("Welcome to " + System.getProperty("github.com"));
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.cheapflights.com");
+        driver.manage().window().maximize();
+
+        try {
+            WebElement kalkis = driver.findElement(By.xpath("//div[@class='zEiP-formField zEiP-origin']"));
+            kalkis.click();Thread.sleep(2000);
+            WebElement kalkisIlGir = driver.findElement(By.xpath("//input[@type='text']"));
+            for (int i=0; i<12; i++){
+                kalkisIlGir.sendKeys(Keys.BACK_SPACE);//Thread.sleep(2000);
+            }
+            kalkisIlGir.sendKeys("Antalya",Keys.ENTER);Thread.sleep(2000);
+
+            WebElement varis = driver.findElement(By.xpath("//div[@class='zEiP-formField zEiP-destination']"));
+            varis.click();Thread.sleep(2000);
+            WebElement varisIlGir = driver.findElement(By.xpath("//input[@type='text']"));
+            varisIlGir.sendKeys("Adana",Keys.ENTER);Thread.sleep(2000);
+
+            WebElement gidisTakvim = driver.findElement(By.xpath("//div[@class='cQtq-input cQtq-mod-responsive']"));
+            gidisTakvim.click();Thread.sleep(2000);
+            WebElement gidisTarihi = driver.findElement(By.xpath("//div[@aria-label='Monday December 25, 2023']"));
+            WebElement donusTarihi = driver.findElement(By.xpath("//div[@aria-label='Wednesday January 24, 2024']"));
+            gidisTarihi.click();Thread.sleep(2000);
+            donusTarihi.click();Thread.sleep(2000);
+
+            WebElement cabinClass = driver.findElement(By.xpath("//div[@class='zEiP-formField zEiP-traveler']"));
+            cabinClass.click();Thread.sleep(2000);
+            driver.findElement(By.xpath("//label[@class='Q9qx-label Q9qx-checked']")).click();Thread.sleep(2000);
+            driver.findElement(By.xpath("/html/body/div[3]/div/div[2]/div[1]/div[5]/div/button[2]")).click();Thread.sleep(2000);
+        }
+        finally {
+            driver.quit();
+        }
+
+
+
+        System.out.println("Hello world!");
     }
 }
